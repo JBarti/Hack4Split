0c35fec9f2bd75fc4d20baf5b809d633f6649b3f
JBarti <josip.bartulovic3@gmail.com>
2020-05-02 18:30:44 +0200

    Add new card form

--- frontend/scripts/subpage.js
+++ frontend/scripts/subpage.js
@@ -76,10 +76,10 @@ function breadcrumbClick() {
     let breadcrumbState = $breadcrumbs.data("state");
 
     const clickedIndex = breadcrumbState.findIndex((nodeData) =>
-        nodeData.id == $clickedBreadcrumb.attr("node-id")
+        nodeData.id == $clickedBreadcrumb.attr("data-node-id")
     );
 
-    breadcrumbState = breadcrumbState.slice(0, clickedIndex+1);
+    breadcrumbState = breadcrumbState.slice(0, clickedIndex + 1);
     $breadcrumbs.data("state", breadcrumbState);
 
     displayCard(breadcrumbState.pop());
@@ -99,7 +99,7 @@ function refreshBreadcrumbs() {
         $link.text(nodeData.title);
         if (index !== indexFinal) {
             $link.attr("href", `#${nodeData.id}`);
-            $link.attr("node-id", nodeData.id);
+            $link.attr("data-node-id", nodeData.id);
             $link.on("click", breadcrumbClick);
 
         } else $link.removeAttr("href");
@@ -157,8 +157,72 @@ function enableSearch() {
 }
 
 
+function triggerFileInput() {
+    const $newFileInput = $($.parseHTML(fileInputTemplate));
+    const $fileInputGroup = $(".file-input-group");
+    $fileInputGroup.append($newFileInput);
+    let currentIndex = $(this).data("currentIndex");
+    $newFileInput
+        .attr("data-file-index", currentIndex)
+        .attr("name", `image-${currentIndex}`)
+        .on("change", fileInputChange)
+        .trigger("click");
+
+
+    $(this).data({"currentIndex": currentIndex + 1});
+    $fileInputGroup.find("input[type=file]").filter(function () {
+        if ($(this)[0] === $newFileInput[0]) return;
+        if (!$(this).val()) $(this).remove();
+    });
+}
+
+
+function removeFile(dataFileIndex) {
+    return function () {
+        $(`div[data-file-index=${dataFileIndex}]`).remove();
+        $(`input[data-file-index=${dataFileIndex}]`).remove();
+    }
+}
+
+
+function fileInputChange() {
+    const $filePreview = $($.parseHTML(filePreviewTemplate));
+    const dataFileIndex = $(this).attr("data-file-index");
+    const file = $(this)[0].files[0];
+    console.log(file);
+    $filePreview
+        .attr("data-file-index", dataFileIndex)
+        .find(".file-name").text(file.name);
+    $filePreview.find(".file-remove").on("click", removeFile(dataFileIndex));
+    $(".file-previews").append($filePreview);
+}
+
+
+function submitNewCardForm() {
+    const $addCardModal = $("#addCardModal");
+    const $fileInputGroup = $addCardModal.find(".file-input-group");
+    $fileInputGroup.find("input[type=file]").filter(function () {
+        if (!$(this).val()) $(this).remove();
+    });
+    $addCardModal.find("form").trigger("submit");
+
+}
+
+function addCardModalSetup() {
+    const $addCardModal = $("#addCardModal");
+    $addCardModal.find(".file-input-trigger")
+        .on("click", triggerFileInput)
+        .data({"currentIndex": 0});
+    $addCardModal.find(".file-input")
+        .on("change", fileInputChange);
+    $addCardModal.find(".form-post")
+        .on("click", submitNewCardForm);
+}
+
+
 $(function () {
     const data = getMapData();
     displayCard(data);
     enableSearch();
+    addCardModalSetup();
 });
